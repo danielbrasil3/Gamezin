56854aa3b589cff9c1cd22ba6189eb5885bb7dad
danielbrasil3 <brasilagar@gmail.com>
2023-10-29 19:07:01 -0300

    receita total & relatorios

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function go()
     var circle = document.querySelector('#circleProgress')
     var circle1 = document.querySelector('#circleProgress1')
     var circle2 = document.querySelector('#circleProgress2')
+    var porcento = document.querySelector('#porcento').value
+    var setasvg = document.querySelector('#seta')
     var number = document.querySelector('#numberProgress').value;
     document.querySelector('.percentage').innerHTML = number + '%'
     document.querySelector('.percentage1').innerHTML = number + '%'
@@ -10,4 +12,21 @@ function go()
     circle.style.strokeDashoffset = 226 - (226 * number) / 100;
     circle1.style.strokeDashoffset = 226 - (226 * number) / 100;
     circle2.style.strokeDashoffset = 226 - (226 * number) / 100;
+    if (porcento <= 0) {
+        setasvg.style.stroke = '#FF0000'
+    } else {
+        setasvg.style.stroke = '#4CE13F'
+    }
 }
+
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+        <td>${order.periodo}</td>
+        <td>${order.margemdelucro}</td>
+        <td>${order.receita}</td>
+        <td>${order.lucro}</td>
+        `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+});
---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        periodo: '17:00 as 18:00',
+        margemdelucro: '30%',
+        receita: 'R$10K',
+        lucro: 'R$5K'
+    },
+    {
+        periodo: '12:00 as 13:00',
+        margemdelucro: '21%',
+        receita: 'R$20K',
+        lucro: 'R$7K'
+    },
+    {
+        periodo: '15:00 as 16:00',
+        margemdelucro: '25%',
+        receita: 'R$5K',
+        lucro: 'R$2K'
+    },
+    {
+        periodo: '17:00 as 18:00',
+        margemdelucro: '30%',
+        receita: 'R$10K',
+        lucro: 'R$5K'
+    },
+    {
+        periodo: '12:00 as 13:00',
+        margemdelucro: '21%',
+        receita: 'R$20K',
+        lucro: 'R$7K'
+    },
+    {
+        periodo: '15:00 as 16:00',
+        margemdelucro: '25%',
+        receita: 'R$5K',
+        lucro: 'R$2K'
+    },
+]
