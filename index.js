478b902e85e1af4bdd052e22d69056c4f6a54338
danielbrasil3 <brasilagar@gmail.com>
2023-10-30 16:26:36 -0300

    Grafico

--- index.js
+++ index.js
@@ -30,3 +30,75 @@ Orders.forEach(order => {
     tr.innerHTML = trContent;
     document.querySelector('table tbody').appendChild(tr);
 });
+
+const areaChartOptions = {
+    series: [
+        {
+            name: "Vendas",
+            data: [100,150,250,300,259,240,560,324,100,70,50,300],
+            zIndex: 0
+        },
+        {
+            name: "Gastos",
+            data: [300,321,450,453,654,213,432,654,213,342,321,432],
+            zIndex: 1
+        }
+    ],
+    chart: {
+        height: "91%",
+        width: "100%",
+        type: 'area',
+        foreColor: "#5D7285",
+        fontFamily: "Poppins",
+        stacked: false,
+        offsetX: -10,
+        offsetY: -80,
+        toolbar: {
+            show: false,
+        },
+    },
+    responsive: [{
+        breakpoint: 1024,
+        options: {},
+    }],
+    legend: {
+        show: false
+    },
+    colors: ['#E3000E', '#4CE13F'],
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: 'smooth',
+    },
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul','Aug','Sep','Oct','Nov','Dec'],
+    
+    tooltip: {
+        shared: true,
+        intersect: false,
+        y: [{
+            formatter: function (y) {
+              if(typeof y !== "undefined") {
+                return "R$ " + y.toFixed(2);
+              }
+              return y;
+              
+            }
+          }, {
+            formatter: function (y) {
+              if(typeof y !== "undefined") {
+                return "R$ " + y.toFixed(2);
+              }
+              return y;
+              
+            }
+          }]
+    },
+  };
+  
+  const areaChart = new ApexCharts(
+    document.querySelector('#area-chart'),
+    areaChartOptions
+  );
+  areaChart.render();
+
